15cfe1819eb765cc4ac47c579b5bd551ba1a210c
Donatas <donkuprys@gmail.com>
2021-10-10 12:02:27 +0300

    task add

--- Hotel.ts
+++ Hotel.ts
@@ -1,1 +1,20 @@
-console.log('Lotus Travel');
+class Hotel {
+    public readonly name: string;
+    public readonly adress: string;
+    public readonly stars: number;
+    public rooms : {}[];
+
+    public constructor(name: string, 
+                    adress: string,
+                    stars: number){
+
+        this.name = name;
+        this.adress = adress;
+        this.stars = stars;
+        this.rooms = [];
+
+    }
+
+    
+
+}
