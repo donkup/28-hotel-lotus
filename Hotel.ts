108d7641e4919552254fd702c781efdd4a7a3bf4
Donatas <donkuprys@gmail.com>
2021-10-12 17:46:38 +0300

    update by paulius

--- Hotel.ts
+++ Hotel.ts
@@ -1,10 +1,9 @@
-// import { Room } from "./Room";
 
 class Hotel {
     public readonly name: string;
     public readonly adress: string;
     public readonly stars: number;
-    public rooms: {}[];
+    public rooms: Room[];
 
     public constructor(name: string,
                  adress: string,
@@ -16,34 +15,57 @@ class Hotel {
         this.rooms = [];
     }
 
-    //spausdinam Hotel info i konsole:
-    public printData(onlyComfort?: boolean): void {
-        if(!onlyComfort){
-            console.log(' ');
-        } else {
-            console.log(`Name: ${this.name}`);
-            console.log(`Adress: ${this.adress}`);
-            console.log(`${this.stars} stars`);
-        }
-    }
-
     // pridedam kambary i masyva:
-    addRoom(room: Room): void {
+    public addRoom(room: Room): void {
         this.rooms.push(room);
     }
-
+    
     //spausdinam kambarius
-    public printRooms(minComfort?: number): void {
-        this.printData(false)
-        console.log(this.rooms);
-        
+    //Version 2->:
+    private printRooms(minComfort?: number): void {
+       for (const room of this.rooms){
+          if (room.comfort > minComfort || minComfort === undefined) {
+              console.log('--------KAMBARYS----------');
+              room.printData();
+          }
+       }   
     }
+    //Version 1->:
+    // private printRooms(minComfort?: number): void {
+    //     let count = 1;
+    //    for (const room of this.rooms){
+           
+    //        if (minComfort !== undefined){    
+    //            if (room.comfort > minComfort) {
+    //                console.log(`***Kambarys numeris ${count++}.***`);
+    //                room.printData()
+    //             }
+    //         } else {
+    //            console.log(`***Kambarys numeris ${count++}.***`);
+    //            room.printData()
+    //        }
+    //    }   
+    // }
 
+    //spausdinam Hotel info i konsole:
+    public printData(onlyComfort?: boolean): void {
+        console.log(`Name: ${this.name}`);
+        console.log(`Adress: ${this.adress}`);
+        console.log(`${this.stars} stars`);
+        
+        const comfortLvl = 15;
+
+        if (onlyComfort) {
+            this.printRooms(comfortLvl);
+        } else {
+            this.printRooms();
+        }
+    }
 }
 
 class Room {
-    size: number;
-    capacity: number;
+    public readonly size: number;
+    public readonly capacity: number;
 
     public constructor(size: number, capacity: number){
       this.size = size;
@@ -56,7 +78,7 @@ class Room {
     }
 
     //spausdinam Room info:
-    public printData(onlyComfort?: boolean): void {
+    public printData(): void {
         console.log(`Kambario dydis: ${this.size} m2.`);
         console.log(`Asmenu kiekis: ${this.capacity} zmones.`);
         console.log(`Komforto lygis: ${this.comfort}.`);
@@ -65,12 +87,14 @@ class Room {
 }
 
 class Spa extends Room {
-    poolSize: number;
-    poolTemperature: number;
+    public readonly poolSize: number;
+    public readonly poolTemperature: number;
     
-    constructor(poolSize: number,
+    constructor(size: number,
+                capacity: number,
+                poolSize: number,
                 poolTemperature: number){
-        super(40, 2)
+        super(size, capacity)
         this.poolSize = poolSize;
         this.poolTemperature = poolTemperature;
     }
@@ -80,35 +104,74 @@ class Spa extends Room {
         return (this.size - this.poolSize)/this.capacity;
     }
 
-    //spausdinam Spa info:
-    public printData(onlyComfort?: boolean): void {
+    //spausdinam Spa Kambario info:
+    public printData(): void {
        super.printData();
        console.log(`Baseino dydis: ${this.poolSize} m2.`);
        console.log(`Vandens temperatura: ${this.poolTemperature} C.`);
-       
     }
 }
 
-//sukuriam viesbuti
-const lotustravel = new Hotel('Lotus Travel', 'Paradise ave.777', 5);
+//EXECUTION BELOW
 
-//spausdinam viesbucio info
-lotustravel.printData(true);
+// //sukuriam viesbuti
+const transylvania = new Hotel('Transylvania', 'Worms Eye 17', 5);
 
-//pridedam kambarius
-lotustravel.addRoom(new Room(30,2));
-lotustravel.addRoom(new Room(50,3));
-lotustravel.addRoom(new Room(20,1));
-lotustravel.addRoom(new Room(100,5));
-console.log('************');
+// //pridedam kambarius
+// transylvania.addRoom(new Room(30, 2));
+// transylvania.addRoom(new Room(25, 4));
+// transylvania.addRoom(new Spa(40, 2, 5, 18));
 
-console.log('Viesbutyje esantys kambariai:')
-lotustravel.printRooms()
+// //spausdinam viesbucio info
+// transylvania.printData();
 
-const dvivietis = new Room(45,2)
-console.log('Dvivietis:', dvivietis);
-dvivietis.printData()
+// console.log(`*********************`);
+// console.log(`tik COMFORT:`);
+// console.log(`*********************`);
 
-const spaRoom = new Spa(15,26)
-console.log('SPA Room:', spaRoom);
-spaRoom.printData()
+// //spausdinam viesbucio info bet tik su kambariais kuriu
+// //Komfortas > 15
+// transylvania.printData(true);
+
+const UI = {
+    roomSize: document.getElementById('size') as HTMLInputElement,
+    capacity: document.getElementById('capacity') as HTMLInputElement,
+    poolSize: document.getElementById('pool-size') as HTMLInputElement,
+    temperature: document.getElementById('water') as HTMLInputElement,
+    button: document.getElementById('button') as HTMLInputElement,
+}
+
+//adding event listener to button to gather data from inputs 
+UI.button.addEventListener('click', () => {
+    const size = Number(UI.roomSize.value); //pakeiciam tipa su number()
+    const capacity = +(UI.capacity.value);// arba su +
+    const poolSize = +(UI.poolSize.value);
+    const poolTemperature = +(UI.temperature.value);
+
+    //Validations
+    if(size === 0 ||
+        capacity === 0){
+            console.error('ERROR: Cannot add Room without size or capacity');
+            return
+    }
+
+    if (size < 0 ||
+        capacity < 0 ||
+        poolSize < 0 ||
+        poolTemperature < 0) {
+        console.error('ERROR: parameter has to be a positive number');
+        return
+    }
+
+    if (poolSize === 0 ||
+        poolTemperature === 0) {
+        
+        transylvania.addRoom(new Room(size, capacity));
+    } else {
+        transylvania.addRoom(new Spa(size, capacity, poolSize, poolTemperature));
+    }
+    transylvania.printData()
+
+    //Render Room Card
+    
+});
--- Task.md
+++ Task.md
@@ -1,25 +1,36 @@
-Panaudodami TypeScript parašykite programą viešbučių tinklui.
+**Panaudodami TypeScript parašykite programą viešbučių tinklui.**
 
-Sukurkite viešbutį ir jam pridėkite bent du paprastus kambarius ir vieną baseiną.
-Atspausdinkite visus viešbučio duomenis (panaudodami viešbučio printData() metodą) ir paskui dar kartą atspausdinkite viešbučio duomenis, tik šįkartą į printData metodo parametrą onlyComfort nurodykite true.
-Klasė HotelAtributai:
-name (string) - viešbučio pavadinimas, priskiriamas sukuriant objektą.
-address (string) - viešbučio adresas, priskiriamas sukuriant objektą.
-stars (number) - viešbučio žvaigždučiųskaičius, priskiriamas sukuriant objektą.
-rooms (Room tipo elementų masyvas) - masyvas, kuris saugo viešbutyje esančias patalpas. Sukuriant objektą yra tuščias.
-Metodai:
-addRoom(room: Room) - metodas, kuris leidžia pridėti prie viešbučio patalpą.
-printRooms(minComfort?: number) (void) - privatus metodas, kuris atspausdina (panaudojant patalpos printData() metodą) visas viešbutyje esančias patalpas. MinComfort parametras nebūtinas perduoti, bet jei jis perduodamas, atspausdinamos tik tos patalpos, kurių komforto sąntykis yra didesnis už minComfort.
-printData(onlyComfort?: boolean) (void) - public metodas, kurio pagalba atspausdinami visi viešbučio duomenys, įskaitant ir patalpas. OnlyComfort parametras nėra būtinas, bet jei jis nurodomas spausdinamos patalpos, kurių komforto santykis yra didesnis už 15.
-KlasėRoomAtributai:
-size (number) - kambario dydis, priskiriamas sukuriant objektą.
-capacity (number) - galimas žmonių kiekis telpantis patalpoje, priskiriamas sukuriant objektą.
-Metodai:
-comfort() (number) - metodas, kuris grąžina komforto santykį, kiek patalpoje vienam žmogui tenka erdvės. T.y. size/capacity.
-printData() (void) - public metodas, kurio pagalba atspausdinami visi patalpos duomenys (size, capacity, comfort).
-Klasė Spa. Ši klasė paveldi klasę Room, tačiau turi papildomų atributų:
-poolSize (number) - nurodo baseino dydį, priskiriamas sukuriant objektą.
-poolTemperature (number) - nurodo baseino vandens šilumą, priskiriamas sukuriant objektą.
-Metodai:
-comfort (number) - kaip ir tėvinės klasės (superklasės) identiškas metodas grąžina komforto santykį, kiek patalpoje vienam žmogui tenka erdvės. Tik skaičiuojamas pagal formulę(size - poolSize) / capacity.
-printData (void) - atspausdina visus patalpos duomenis panaudodamas superklasės metodątuo pačiu pavadinimu ir papildomai atspausdina baseino dydįir vandens temperatūrą.
+- _Sukurkite viešbutį ir jam pridėkite bent du paprastus kambarius ir vieną baseiną._
+- Atspausdinkite visus viešbučio duomenis (panaudodami viešbučio printData() metodą) ir paskui dar kartą atspausdinkite viešbučio duomenis, tik šį kartą į printData metodo parametrą _onlyComfort_ nurodykite **true**.
+- _**Klasės Hotel Atributai:**_
+
+  - > **name** (string) - viešbučio pavadinimas, priskiriamas sukuriant objektą.
+  - > **address** (string) - viešbučio adresas, priskiriamas sukuriant objektą.
+  - > **stars** (number) - viešbučio žvaigždučiųskaičius, priskiriamas sukuriant objektą.
+  - > **rooms** (Room tipo elementų masyvas) - masyvas, kuris saugo viešbutyje esančias patalpas. Sukuriant objektą yra tuščias.
+
+    **_Metodai:_**
+
+    - **addRoom**(room: Room) - metodas, kuris leidžia pridėti prie viešbučio patalpą.
+    - **printRooms**(minComfort?: number) (void) - privatus metodas, kuris atspausdina (panaudojant patalpos printData() metodą) visas viešbutyje esančias patalpas. **MinComfort** parametras nebūtinas perduoti, bet jei jis perduodamas, atspausdinamos tik tos patalpos, kurių komforto sąntykis yra didesnis už minComfort.
+    - **printData**(onlyComfort?: boolean) (void) - public metodas, kurio pagalba atspausdinami visi viešbučio duomenys, įskaitant ir patalpas. **OnlyComfort** parametras nėra būtinas, bet jei jis nurodomas spausdinamos patalpos, kurių komforto santykis yra didesnis už _15_.
+
+* **_Klasė Room Atributai:_**
+
+  - > **size** (number) - kambario dydis, priskiriamas sukuriant objektą.
+  - > **capacity** (number) - galimas žmonių kiekis telpantis patalpoje, priskiriamas sukuriant objektą.
+
+  - **_Metodai:_**
+    - **comfort**() (number) - metodas, kuris grąžina komforto santykį, kiek patalpoje vienam žmogui tenka erdvės. T.y. size/capacity.
+    - **printData**() (void) - public metodas, kurio pagalba atspausdinami visi patalpos duomenys (size, capacity, comfort).
+
+* **_Klasė Spa_**.
+
+  Ši klasė paveldi klasę **Room**, tačiau turi papildomų _atributų:_
+
+  - > **poolSize** (number) - nurodo baseino dydį, priskiriamas sukuriant objektą.
+  - > **poolTemperature** (number) - nurodo baseino vandens šilumą, priskiriamas sukuriant objektą.
+  - **_Metodai:_**
+
+    - **comfort** (number) - kaip ir tėvinės klasės (superklasės) identiškas metodas grąžina komforto santykį, kiek patalpoje vienam žmogui tenka erdvės. Tik skaičiuojamas pagal formulę(size - poolSize) / capacity.
+    - **printData** (void) - atspausdina visus patalpos duomenis panaudodamas superklasės metodą tuo pačiu pavadinimu ir papildomai atspausdina baseino dydį ir vandens temperatūrą.
